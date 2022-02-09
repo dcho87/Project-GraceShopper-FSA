d8ab463c8e06957b756d61fa5ec85616b073002f
DC <sbzdcb@gmail.com>
2022-02-08 19:00:46 -0500

    commit before merge

--- server/api/orders.js
+++ server/api/orders.js
@@ -2,9 +2,39 @@ const router = require("express").Router();
 const { models: { User, Product, Order, CompletedOrder }} = require('../db')
 module.exports = router
 
+
+// get all orders
 router.get('/', async (req, res, next) => {
     try {
         res.send(await Order.findAll())
+    }
+    catch(ex){
+        next(ex)
+    }
+})
+
+// get order by order id 
+router.get('/:id', async (req, res, next) => {
+    try{
+        let order = await Order.findByPk(req.params.id, {
+            include: Product
+        })
+        res.send(order)
+    }
+    catch(ex){
+        next(ex)
+    }
+})
+
+// add a product to cart
+router.put('/:id', async (req, res, next) => {
+    try {
+        const order = await Order.findByPk(req.params.id)
+        const productId = req.body.id
+        
+        const product = await Product.findByPk(productId)
+
+
     }
     catch(ex){
         next(ex)
