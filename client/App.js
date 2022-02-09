3827132804ecff4968a533973b6a10ad22f5883a
Joe Collins <jpatcollins@gmail.com>
2022-02-09 12:52:46 -0600

    nothing needed from this commit

--- client/App.js
+++ client/App.js
@@ -2,16 +2,30 @@ import React from "react";
 import { HashRouter as Router } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Routes from "./Routes";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = store.getState();
+  }
 
-const App = () => {
-  return (
-    <Router>
-      <div className="main">
-        <Navbar />
-        <Routes />
-      </div>
-    </Router>
-  );
-};
+  async componentDidMount() {
+    store.dispatch(init());
+
+    store.subscribe(() => {
+      this.setState(store.getState());
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="main">
+          <Navbar />
+          <Routes />
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
