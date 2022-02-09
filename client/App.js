cb1a285273f87bc717c0eef0abf4feddd2fdc560
Joe Collins <jpatcollins@gmail.com>
2022-02-09 12:57:24 -0600

    no changes needed from this commit

--- client/App.js
+++ client/App.js
@@ -1,17 +1,10 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 import { HashRouter as Router } from "react-router-dom";
-import { fetchProducts } from "./store/product_store";
 import Navbar from "./components/navbar/Navbar";
 import Routes from "./Routes";
 import "./App.css";
 
 const App = async () => {
-  const [products, setProducts] = useState([]);
-  const dispatch = useDispatch();
-  await setProducts(dispatch(fetchProducts()));
-
-  console.log(products);
   return (
     <Router>
       <div className="main">
