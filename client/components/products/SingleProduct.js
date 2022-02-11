5d91b87e9fccee96662baf01440eacbf685d6f1b
saadk1580 <saadk1580@gmail.com>
2022-02-10 19:48:08 -0500

    s

---
+++ client/components/products/SingleProduct.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useReducer, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProducts, products } from "../../store/product_store";
+
+const SingleProduct = () => {
+  const dispatch = useDispatch();
+
+  //   const [state_, setstate] = useState(0);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
+  const state = useSelector((state) => state);
+
+  console.log(state.pictureURL);
+
+  return (
+    <div className="products-container">
+      {state.products.map((product) => (
+        <div className="cla">
+          <h1>{product.name}</h1>
+          <img src={product.imageURL} />
+          <p>{product.price}</p>
+          <p>{product.inventory}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SingleProduct;
