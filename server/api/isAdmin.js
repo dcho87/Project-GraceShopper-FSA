13c5a2f12a2e537af9a7a99b48a8d059ed78b886
DC <sbzdcb@gmail.com>
2022-02-11 16:35:12 -0500

    added isAdmin middleware to api

---
+++ server/api/isAdmin.js
@@ -0,0 +1,9 @@
+const isAdmin = async (req, res, next) => {
+    const user = await req.user
+    if(user && user.isAdmin) {
+        next()
+    } else {
+        res.status(401).send('No Access To This Page')    }
+}
+
+module.exports = {isAdmin}
