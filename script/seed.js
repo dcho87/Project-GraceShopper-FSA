67f6d68f52cebe62da99acba9c08746ccaa1c1ab
DC <sbzdcb@gmail.com>
2022-02-11 16:55:26 -0500

    added isAdmin middleware

--- script/seed.js
+++ script/seed.js
@@ -62,15 +62,9 @@ async function seed() {
       description: "A beautiful seascape",
       imageURL:
         "https://images.unsplash.com/photo-1518837695005-2083093ee35b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-<<<<<<< HEAD
-      category: "Landscape",
-      price: 50,
-      inventory: 12,
-=======
       category: "Landscapes",
       price: 39,
       inventory: 12
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -78,15 +72,9 @@ async function seed() {
       description: "A spanning desert",
       imageURL:
         "https://media.istockphoto.com/photos/sand-dunes-in-the-sahara-desert-morocco-picture-id983422208?k=20&m=983422208&s=612x612&w=0&h=gjtn_hlkqzKfGQJQg62QTUbpFlQ0O5E5LnxQ5arh4ao=",
-<<<<<<< HEAD
-      category: "Landscape",
-      price: 50,
-      inventory: 18,
-=======
       category: "Landscapes",
       price: 22,
       inventory: 18
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -94,15 +82,9 @@ async function seed() {
       description: "A stunning snowy mountain",
       imageURL:
         "https://media.istockphoto.com/videos/the-perfect-mountain-aerial-shot-video-id181013019?s=640x640",
-<<<<<<< HEAD
-      category: "Landscape",
-      price: 50,
-      inventory: 19,
-=======
       category: "Landscapes",
       price: 25,
       inventory: 19
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -110,15 +92,9 @@ async function seed() {
       description: "A captivating silent forest",
       imageURL:
         "https://www.treehugger.com/thmb/QolJfOYFmxwIH6Sxv5SBqY8Kq-M=/1885x1414/smart/filters:no_upscale()/GettyImages-1273584292-cbcd5f85f4c646d58f7a7fa158dcaaeb.jpg",
-<<<<<<< HEAD
-      category: "Landscape",
-      price: 50,
-      inventory: 15,
-=======
       category: "Landscapes",
       price: 39,
       inventory: 15
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -137,15 +113,9 @@ async function seed() {
       description: "Unique Squiggle with two peaks",
       imageURL:
         "https://lh3.googleusercontent.com/36pq45ZwWmyX1haBqD29ysVcw8F9Cmx1ZgAFC6ptJfnDAJneA9Vqk6pRSScZDU0c8Hh1MPF6uvEcNEI-cT_DEbyWNBGhvinwA5DE=w1400-k",
-<<<<<<< HEAD
-      category: "Doodle",
-      price: 70,
-      inventory: 7,
-=======
       category: "Doodles",
       price: 170,
       inventory: 7
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -153,15 +123,9 @@ async function seed() {
       description: "Chromatic and intense action",
       imageURL:
         "https://lh3.googleusercontent.com/_gy7-sZJ5u5H0dBkANZTtGaBtbBgU_Ui424RHvx9k1AhffIajWs-ttf7Dn4sUcW2fdmhEN0Hcjn6KamobkrAs5hVtBMf29xzNSSDOw=w1400-k",
-<<<<<<< HEAD
-      category: "Doodle",
-      price: 70,
-      inventory: 5,
-=======
       category: "Doodles",
       price: 120,
       inventory: 5
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -169,15 +133,9 @@ async function seed() {
       description: "Scintillating vertical lines",
       imageURL:
         "https://lh3.googleusercontent.com/V8vm4TZJuQDB-frpp2RROJJBcQg2A8KC-nTf7ZMM04RDlhlCPDDs0cR0gtyWV2llJ0yR9L9W6zD0Ra7jnepWExECoNs5ZIfL7bfsAA=w1400-k",
-<<<<<<< HEAD
-      category: "Doodle",
-      price: 70,
-      inventory: 8,
-=======
       category: "Doodles",
       price: 290,
       inventory: 8
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -185,15 +143,9 @@ async function seed() {
       description: "Vibrate experience",
       imageURL:
         "https://static.nftgo.io/asset/metadata/c9fba9f0b9225caa73710b35b8ad0eb8.png",
-<<<<<<< HEAD
-      category: "Doodle",
-      price: 70,
-      inventory: 123,
-=======
       category: "Doodles",
       price: 350,
       inventory: 123
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -201,15 +153,9 @@ async function seed() {
       description: "Lighter hue for a more mellow affair",
       imageURL:
         "https://external-preview.redd.it/cZn2m1ikRJMIvOWLjl6p-hT6vmOMEjVIc-uPUQxgO2A.jpg?auto=webp&s=0821e1f4ae5004e14320138c67d50306f7c5d961",
-<<<<<<< HEAD
-      category: "Doodle",
-      price: 70,
-      inventory: 4,
-=======
       category: "Doodles",
       price: 99,
       inventory: 4
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     //cars
@@ -219,11 +165,7 @@ async function seed() {
       description: "It goes fast enough",
       imageURL:
         "https://oceanatlanticrentals.com/wp-content/uploads/2016-Club-Car-Precedent-4-Seater-Macon-GA-1295_2.aiimg-w700ar1cr0.jpg",
-<<<<<<< HEAD
-      category: "Car",
-=======
       category: "Cars",
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
       price: 100,
       inventory: 21,
     }),
@@ -233,15 +175,9 @@ async function seed() {
       description: "Luxury and Utility",
       imageURL:
         "https://di-uploads-pod4.dealerinspire.com/sunrisechevyredesign/uploads/2019/02/tahoe-exterior.jpg",
-<<<<<<< HEAD
-      category: "Car",
-      price: 100,
-      inventory: 12,
-=======
       category: "Cars",
       price: 120,
       inventory: 12
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -249,15 +185,9 @@ async function seed() {
       description: "Truck will fit your golf clubs",
       imageURL:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Challenger2-Bergen-Hohne-Training-Area-2.jpg/600px-Challenger2-Bergen-Hohne-Training-Area-2.jpg",
-<<<<<<< HEAD
-      category: "Car",
-      price: 100,
-      inventory: 9,
-=======
       category: "Cars",
       price: 450,
       inventory: 9
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -265,15 +195,9 @@ async function seed() {
       description: 'Suitable for a Bond villan',
       imageURL:
         "https://www.denverpost.com/wp-content/uploads/2021/07/7.14D-Lucid-scaled-1.jpg?w=1020",
-<<<<<<< HEAD
-      category: "Car",
-      price: 100,
-      inventory: 5,
-=======
       category: "Cars",
       price: 720,
       inventory: 5
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     Product.create({
@@ -281,15 +205,9 @@ async function seed() {
       description: "Holding this token gives you fractional ownership of the product",
       imageURL:
         "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/mercedes-amg-c63-cabriolet.jpg?itok=mY23jB1D",
-<<<<<<< HEAD
-      category: "Car",
-      price: 100,
-      inventory: 3,
-=======
       category: "Cars",
       price: 399,
       inventory: 3
->>>>>>> 0a05fbd394bb8623f6c515b07ae674fc22ed0872
     }),
 
     // kangaroo
