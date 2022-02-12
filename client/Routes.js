e4d572191037a06789cadbf008a56f9eac0da35f
saadk1580 <saadk1580@gmail.com>
2022-02-11 19:42:58 -0500

    commit

--- client/Routes.js
+++ client/Routes.js
@@ -6,13 +6,13 @@ import Login_Page from "./components/Login_Page";
 import SignUp_Page from "./components/SignUp_Page";
 import Products from "./components/products/Products";
 import Product from "./components/products/Product";
-import Animals from "./components/categories/Animals";
-import Apes from "./components/categories/Apes";
-import Cars from "./components/categories/Cars";
-import Doodles from "./components/categories/Doodles";
-import Landscapes from "./components/categories/Landscapes";
-import Punks from "./components/categories/Punks";
-import Stick_Figures from "./components/categories/Stick_Figures";
+// import Animals from "./components/categories/Animals";
+// import Apes from "./components/categories/Apes";
+// import Cars from "./components/categories/Cars";
+// import Doodles from "./components/categories/Doodles";
+// import Landscapes from "./components/categories/Landscapes";
+// import Punks from "./components/categories/Punks";
+// import Stick_Figures from "./components/categories/Stick_Figures";
 import Cart from "./components/cart/Cart";
 import Bio from "./components/bio/Bio";
 
@@ -30,13 +30,13 @@ const Routes = () => {
       {isLoggedIn ? (
         <Switch>
           <Route path="/home" component={Products} />
-          <Route path="/products/kangaroos" component={Animals} />
+          {/* <Route path="/products/kangaroos" component={Animals} />
           <Route path="/products/apes" component={Apes} />
           <Route path="/products/cars" component={Cars} />
           <Route path="/products/doodles" component={Doodles} />
           <Route path="/products/landscapes" component={Landscapes} />
           <Route path="/products/punks" component={Punks} />
-          <Route path="/products/people" component={Stick_Figures} />
+          <Route path="/products/people" component={Stick_Figures} /> */}
           <Route path="/products/:id" component={Product} exact />
           <Route path="/cart" component={Cart} />
           <Route path="/bio" component={Bio} />
