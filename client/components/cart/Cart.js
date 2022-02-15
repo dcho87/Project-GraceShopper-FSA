4ac14acccc86702c56d0cf037449eec91d4e14ba
bengreenspan <91162661+bengreenspan@users.noreply.github.com>
2022-02-15 13:32:43 -0500

    user with no products added

--- client/components/cart/Cart.js
+++ client/components/cart/Cart.js
@@ -23,52 +23,81 @@ const Cart = () => {
   if (!orderDetails.products) {
     return null;
   }
-
+  console.log(user);
   console.log("orderDetails", orderDetails);
-
+  console.log(orderDetails.totalItems);
   return (
-    <div className="cart-cont">
-      <div className="header">
-        <h1>Shopping Cart</h1>
-        <Link to="/orders/previous_orders">View Previous Orders</Link>
-      </div>
-      {orderDetails.products.map((product) => (
-        <div key={product.id} className="single-product-cont">
-          <Link to={`/products/${product.id}`}>
-            <img className="cart-image" src={product.imageURL}></img>
-          </Link>
-          <div className="order-info-cont">
-            <Link to={`/products/${product.id}`}>
-              <h3> Name: {product.name}</h3>
-              <h4>Description: {product.description}</h4>
+    <div>
+      {/* <nav> */}
+      {/* {isLoggedIn ? ( */}
+      {/* <div className="nav-links"> */}
+      {/* The navbar will show these links after you log in */}
+      {/* </div> */}
+      {/* ) : ( */}
+      {/* <div> */}
+      {/* The navbar will show these links before you log in */}
+      {/* )} */}
+      {/* </nav> */}
+      {!!orderDetails.totalItems ? (
+        <div className="cart-cont">
+          <div className="header">
+            <h1>Shopping Cart</h1>
+            <Link to="/orders/previous_orders">View Previous Orders</Link>
+          </div>
+          {orderDetails.products.map((product) => (
+            <div key={product.id} className="single-product-cont">
+              <Link to={`/products/${product.id}`}>
+                <img className="cart-image" src={product.imageURL}></img>
+              </Link>
+              <div className="order-info-cont">
+                <Link to={`/products/${product.id}`}>
+                  <h3> Name: {product.name}</h3>
+                  <h4>Description: {product.description}</h4>
+                </Link>
+                {product.inventory} left in stock.
+                <div className="quantity-cont">
+                  <div>Order Quantity:</div>
+                  <input
+                    type="number"
+                    step={1}
+                    placeholder={product.orderproduct.itemCount}
+                    min={0}
+                    max={product.inventory}
+                  ></input>
+                  <button>Update Quantity</button>
+                  <button>Remove from Order</button>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <div className="checkout-cont">
+            <div className="total-cont">
+              Subtotal ({orderDetails.totalItems} items): $
+              {orderDetails.totalPrice}
+            </div>
+            <Link to="/orders/checkout" className="link-to-checkout-cont">
+              Proceed to Checkout
             </Link>
-            {product.inventory} left in stock.
-            <div className="quantity-cont">
-              <div>Order Quantity:</div>
-              <input
-                type="number"
-                step={1}
-                placeholder={product.orderproduct.itemCount}
-                min={0}
-                max={product.inventory}
-              ></input>
-              <button>Update Quantity</button>
-              <button>Remove from Order</button>
+          </div>
+          <Link to="/home">Continue Shopping</Link>
+        </div>
+      ) : (
+        <div>
+          <div className="cart-cont">
+            <div className="header">
+              <h1>Shopping Cart</h1>
+              <Link to="/orders/previous_orders">View Previous Orders</Link>
             </div>
           </div>
-        </div>
-      ))}
 
-      <div className="checkout-cont">
-        <div className="total-cont">
-          Subtotal ({orderDetails.totalItems} items): ${orderDetails.totalPrice}
+          <h2>
+            {user.first_name}, you have nothing in your cart. Click
+            <Link to="/home"> here to</Link> add products
+          </h2>
+          <Link to="/home">Continue Shopping</Link>
         </div>
-        <Link to="/orders/checkout" className="link-to-checkout-cont">
-          Proceed to Checkout
-        </Link>
-      </div>
-
-      <Link to="/home">Continue Shopping</Link>
+      )}
     </div>
   );
 };
