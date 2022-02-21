24b5c68e3bf0999e87ae9c3e062ae34974b0d90e
bengreenspan <91162661+bengreenspan@users.noreply.github.com>
2022-02-21 18:15:11 -0500

    pagination working, styled poorly

--- client/components/products/Products.js
+++ client/components/products/Products.js
@@ -9,11 +9,14 @@ import {
 import { Link } from "react-router-dom";
 import "./Products.css";
 import Product_Edit from "./Product_Edit.js";
+import Pagination from "./Pagination.js";
 
 const Products = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
   const user = state.auth;
+  const [currentPage, setCurrentPage] = useState(4);
+  const [productsPerPage] = useState(5);
 
   useEffect(() => {
     dispatch(fetchOrderDetails(user));
@@ -38,79 +41,95 @@ const Products = () => {
     productId,
   };
 
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexofFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = state.products.slice(
+    indexofFirstProduct,
+    indexOfLastProduct
+  );
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="products-container">
-      {state.products.map((product) => (
-        <div className="product" key={product.name}>
-          <div className="img-div">
-            <Link to={`/products/${product.id}`}>
-              <img className="product-img" src={product.imageURL} />
-            </Link>
-          </div>
-
-          <div className="product-details">
-            <p>
-              <b> Top Bid:</b> ${product.price}
-            </p>
-            <p>
-              {" "}
-              <b>Left in stock:</b> {product.inventory}
-            </p>
-            <p>
-              <b>Category: </b>
-              <Link to={`/products/${product.category}`}>
-                {product.category}
+    <div>
+      <div className="products-container">
+        {currentProducts.map((product) => (
+          <div className="product" key={product.name}>
+            <div className="img-div">
+              <Link to={`/products/${product.id}`}>
+                <img className="product-img" src={product.imageURL} />
               </Link>
-            </p>
-            <button
-              disabled={product.id !== productId || totalItems === 0}
-              onClick={(ev) => {
-                dispatch(addToOrder(orderToAdd, user));
-                dispatch(editProduct(orderToAdd, product));
-                setProductId("");
-              }}
-            >
-              Add to cart
-            </button>
-            <input
-              type="number"
-              step={1}
-              placeholder={0}
-              min={0}
-              max={product.inventory}
-              onChange={(ev) => {
-                setTotalItems(ev.target.value * 1);
-                setTotalPrice(ev.target.value * product.price);
-                setProductId(product.id);
-              }}
-            ></input>
+            </div>
 
-            {user.isAdmin === true ? (
-              <div>
+            <div className="product-details">
+              <p>
+                <b> Top Bid:</b> ${product.price}
+              </p>
+              <p>
                 {" "}
-                <Link to={`/products/edit/${product.id}`}>
+                <b>Left in stock:</b> {product.inventory}
+              </p>
+              <p>
+                <b>Category: </b>
+                <Link to={`/products/${product.category}`}>
+                  {product.category}
+                </Link>
+              </p>
+              <button
+                disabled={product.id !== productId || totalItems === 0}
+                onClick={(ev) => {
+                  dispatch(addToOrder(orderToAdd, user, product));
+                  dispatch(editProduct(orderToAdd, product));
+                  setProductId("");
+                }}
+              >
+                Add to cart
+              </button>
+              <input
+                type="number"
+                step={1}
+                placeholder={0}
+                min={0}
+                max={product.inventory}
+                onChange={(ev) => {
+                  setTotalItems(ev.target.value * 1);
+                  setTotalPrice(ev.target.value * product.price);
+                  setProductId(product.id);
+                }}
+              ></input>
+
+              {user.isAdmin === true ? (
+                <div>
+                  {" "}
+                  <Link to={`/products/edit/${product.id}`}>
+                    <button
+                      onClick={() => {
+                        setProductId(product.id);
+                      }}
+                    >
+                      Edit
+                    </button>
+                  </Link>
                   <button
                     onClick={() => {
-                      setProductId(product.id);
+                      dispatch(destroyProduct(product.id));
                     }}
                   >
-                    Edit
+                    Delete
                   </button>
-                </Link>
-                <button
-                  onClick={() => {
-                    dispatch(destroyProduct(product.id));
-                  }}
-                >
-                  Delete
-                </button>
-              </div>
-            ) : (
-              ""
-            )}
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <Pagination
+        productsPerPage={productsPerPage}
+        totalProducts={state.products.length}
+        paginate={paginate}
+      />
     </div>
   );
 };
